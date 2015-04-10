e4c91d7372a5f6bf37128afdf6ebfd4f380bc4cd
BHussain <b_hussain@hotmail.nl>
2015-04-10 16:29:50 +0200

    awesome push

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,15 @@
 package com.mycompany.app;
 
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        new MongoClient("localhost", 27017);
     }
 }
